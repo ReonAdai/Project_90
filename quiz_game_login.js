badf169020cabc1b8724989ca988fb07aa7bbb92
ReonAdai <113380691+ReonAdai@users.noreply.github.com>
2023-06-26 16:40:49 +0100

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,7 +1,7 @@
-function addUser(){
+function adduser(){
     p1_name = document.getElementById("player1_input").value;
     p2_name = document.getElementById("player2_input").value;
     localStorage.setItem("player1_key", p1_name);
     localStorage.setItem("player2_key", p2_name);
-    window.location = "game_page.html";
+    window.location = "quiz_game_page.html";
 }
